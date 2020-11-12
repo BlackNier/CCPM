07ef2e79939930266e50c135377d99cab6dd224b
IrisTc <1010939908@qq.com>
2020-11-12 13:08:30 +0800

    修改左侧菜单栏

--- app/src/main/java/com/iris/ccpm/ui/home/HomeFragment.java
+++ app/src/main/java/com/iris/ccpm/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,11 +24,11 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final ListView lvNews = root.findViewById(R.id.lv_news);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
--- app/src/main/java/com/iris/ccpm/ui/my/MyViewModel.java
+++ app/src/main/java/com/iris/ccpm/ui/my/MyViewModel.java
@@ -1,14 +1,14 @@
-package com.iris.ccpm.ui.gallery;
+package com.iris.ccpm.ui.my;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class MyViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public MyViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
